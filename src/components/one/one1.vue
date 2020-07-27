3091a6a0057019b7b1c8aad1b5137f09c6697e53
李朋 <15646507519@sian.cn>
2020-07-27 14:51:48 +0800

    f

--- src/components/one/one1.vue
+++ src/components/one/one1.vue
@@ -12,6 +12,7 @@
 
 <script>
 export default {
+  // https://www.cnblogs.com/mark5/p/11603428.html
   model: {
     prop: "value", // 定义v-bind的属性，v-bind默认是v-bind:value形式，所以这里用prop: "value"
     event: "changed", // 定义给父组件反馈值的方法，v-on绑定的事件
